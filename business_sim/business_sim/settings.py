9b70cf8e5b9c9183a71985a51e4e3a414a75dcc2
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-15 06:29:13 -0400

    Add basic models

--- business_sim/business_sim/settings.py
+++ business_sim/business_sim/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'store',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
--- business_sim/store/models.py
+++ business_sim/store/models.py
@@ -1,19 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 
-class Products(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(default=0.0)
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    time_in = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def wait_time(self):
+        return timezone.now() - self.time_in
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.CharField(max_length=50)
     amount = models.FloatField(default=0.0)
+    price = models.FloatField(default=0.0)
 
+    def __str__(self):
+        return f'#{self.id} for {self.amount} lbs of {self.product}'
 
-class Baskets(models.Model):
-    cost = models.FloatField(default=0.0)
-    products = models.ManyToManyField(Products, related_name='baskets')
-
-
-class Customers(models.Model):
-    name = models.CharField(max_length=255)
-    time_in = models.DateTimeField(auto_now_add=True)
-    time_out = models.DateTimeField(blank=True)
-    basket = models.ForeignKey(Baskets, on_delete=models.CASCADE)
+    def get_total_cost(self):
+        return f'${self.price * self.amount}'
