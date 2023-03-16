7c264caa1287a67e14d63210d14010335cf756bc
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-16 03:48:02 -0400

    Add order detail and fulfill, customer detail views

--- business_sim/store/urls.py
+++ business_sim/store/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),  # displays orders
+    path('<int:order_id>/', views.order_detail, name='order_detail'),
+    path('<int:order_id>/fulfill/', views.fulfill, name='fulfill'),
+    path('customer/<int:customer_id>/', views.customer, name='customer'),
 ]
--- business_sim/store/views.py
+++ business_sim/store/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("You're at store's index page.")
+
+
+def order_detail(request, order_id):
+    return HttpResponse("You're viewing order %s detail" % order_id)
+
+
+def fulfill(request, order_id):
+    response = "This is order %s fulfillment page"
+    return HttpResponse(response % order_id)
+
+
+def customer(request, customer_id):
+    return HttpResponse("You're viewing customer %s detail" % customer_id)
