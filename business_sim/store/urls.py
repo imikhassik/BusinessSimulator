144d36a9965339acb9e5be54f6b139d505e64b8c
Ilya Mikhasik <imikhassik@gmail.com>
2023-03-17 04:06:36 -0400

    Namespace url names

--- business_sim/store/urls.py
+++ business_sim/store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),  # displays all customers with orders
     path('<int:order_id>/', views.order_detail, name='order_detail'),
